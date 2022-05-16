b999d372712c2fde97436587dab183ea189155b3
José Luis Mandujano <57630829+jose-luis-87@users.noreply.github.com>
2022-05-16 04:53:29 -0500

    * update marcas

--- modules/Marcas.tsx
+++ modules/Marcas.tsx
@@ -40,6 +40,7 @@ let sliderRef = useRef<any>()
                     <Brand img={'/img/logo-naveri.png'} txt={'Tienda estilo boutique que nace en el 2004 en la ciudad de Zamora Michoacán, enfocada en moda y tecnología para adultos jóvenes pendientes de tendencias, ofreciendo las mejores marcas con facilidades de pago. Actualmente cuenta con catálogo de sobre pedido en artículos para el hogar.'}/>
                     <Brand img={'/img/celerity-logo.png'} txt={'Tienda Distribuidor Autorizado Telcel que busca conectar a las personas, ofreciendo los modelos de smartphones más buscados con los mejores precios. Actualmente contamos con 5 sucursales en el estado de Colima con presencia en los municipios de: Colima, Villa de Álvarez y Tecomán.'}/>
                     <Brand img={'/img/logomxl.png'} txt={'Tienda outlet nacida en 2020 en la ciudad de Tlaquepaque, Jalisco con mercancía de marcas reconocidas en ropa, calzado, accesorios, tecnología y artículos para el hogar con precios  reducidos y descuentos permanentes.'}/>
+                    <Brand img={'/img/logo-progresa.png'} txt={'Es una sociedad financiera popular autorizada por la Comisión Nacional Bancaria de Valores, nacida en 1999 en Acámbaro, Guanajuato. Actualmente cuenta con 11 sucursales ubicadas en Guanajuato, Jalisco y Colima. Ofrece productos financieros de captación para ahorro e inversión a plazo fijo y de colocación de crédito comercial y de consumo, todos con las características que le permite la regulación aplicable emitida por la CNBV.'}/>
                 </Carousel>
             </div>
          </div>
